196ceab694b3cf499de2fb291528786834cc7ea1
vivlong <vivlonglz@gmail.com>
2016-04-27 16:19:25 +0800

    Update BAT

--- TMS/release-build.bat
+++ TMS/release-build.bat
@@ -8,10 +8,10 @@ cd platforms\android\build\outputs\apk
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../../my-release-key.keystore android-armv7-release-unsigned.apk alias_name
 
-E:\Android\sdk\android-sdk\build-tools\22.0.1\zipalign.exe -v 4 android-armv7-release-unsigned.apk DMS.apk
+E:\Android\sdk\android-sdk\build-tools\22.0.1\zipalign.exe -v 4 android-armv7-release-unsigned.apk TMS.apk
 
-xcopy /y "%~dp0platforms\android\build\outputs\apk\DMS.apk" "%~dp0"
+xcopy /y "%~dp0platforms\android\build\outputs\apk\TMS.apk" "%~dp0"
 
-del "%~dp0platforms\android\build\outputs\apk\DMS.apk" /f /q
+del "%~dp0platforms\android\build\outputs\apk\TMS.apk" /f /q
 
 pause
