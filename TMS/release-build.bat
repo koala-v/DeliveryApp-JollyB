10b700938e7354644d607be1acfc8b9e552b9b27
koala-v <1904579228@qq.com>
2016-04-29 14:30:34 +0800

    v1.0.1

--- TMS/release-build.bat
+++ TMS/release-build.bat
@@ -8,7 +8,7 @@ cd platforms\android\build\outputs\apk
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../../my-release-key.keystore android-armv7-release-unsigned.apk alias_name
 
-E:\Android\sdk\android-sdk\build-tools\22.0.1\zipalign.exe -v 4 android-armv7-release-unsigned.apk DMS.apk
+C:\Android\sdk\android-sdk\build-tools\22.0.1\zipalign.exe -v 4 android-armv7-release-unsigned.apk DMS.apk
 
 xcopy /y "%~dp0platforms\android\build\outputs\apk\DMS.apk" "%~dp0"
 
