d7d5536ac18182f30ae8eaebd4e185ee2411d8bc
vivlong <vivlonglz@gmail.com>
2016-07-06 15:06:25 +0800

    update dailycompleted

--- TMS/www/view/dailycompleted/dailycompleted.js
+++ TMS/www/view/dailycompleted/dailycompleted.js
@@ -1,89 +1,67 @@
 'use strict';
-app.controller('dailycompletedCtrl', ['ENV', '$scope', '$state', '$ionicPopup', '$cordovaKeyboard', '$cordovaBarcodeScanner', 'ACCEPTJOB_ORM', 'ApiService', '$cordovaSQLite', '$ionicPlatform', 'ionicDatePicker',
-  function(ENV, $scope, $state, $ionicPopup, $cordovaKeyboard, $cordovaBarcodeScanner, ACCEPTJOB_ORM, ApiService, $cordovaSQLite, $ionicPlatform, ionicDatePicker) {
+app.controller( 'dailycompletedCtrl', [ 'ENV', '$scope', '$state', '$ionicPopup', '$cordovaKeyboard', '$cordovaBarcodeScanner', 'ACCEPTJOB_ORM', 'ApiService', '$cordovaSQLite', '$ionicPlatform', 'ionicDatePicker',
+  function( ENV, $scope, $state, $ionicPopup, $cordovaKeyboard, $cordovaBarcodeScanner, ACCEPTJOB_ORM, ApiService, $cordovaSQLite, $ionicPlatform, ionicDatePicker ) {
     var alertPopup = null,
       dataResults = new Array();
+    $scope.Csbk1s = [];
     $scope.Search = {
-      CompletedDate: '',
+      CompletedDate: moment( new Date() ).format( 'YYYYMMDD' ),
       allCompletedDates: []
     };
-     $scope.Csbk1s = [];
-     $scope.Search.CompletedDate = moment(new Date()).format('YYYYMMDD');
-    var ShowDailyCompleted = function() {
-      $ionicPlatform.ready(function() {
-        if (!ENV.fromWeb) {
-          $cordovaSQLite.execute(db, "SELECT * FROM Csbk1  where  DriverId='" + sessionStorage.getItem("strDriverId") + "' and CompletedDate='" + $scope.Search.CompletedDate + "' ")
-            .then(
-              function(results) {
-                  $scope.Csbk1s= new Array();
-                if (results.rows.length > 0) {
-                  var jobs = '';
-                  for (var i = 0; i < results.rows.length; i++) {
-                    var Csbk1_acc = results.rows.item(i);
-                    jobs = {
-                      bookingno: Csbk1_acc.BookingNo,
-                      JobNo: Csbk1_acc.JobNo,
-                      CollectedAmt: Csbk1_acc.CollectedAmt
-                    };
-                    $scope.Csbk1s.push(jobs);
-                  }
-                  if (window.cordova && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.close();
-                  }
-                } else {}
-              },
-              function(error) {}
-            );
-        } else {}
-      });
-    };
-    ShowDailyCompleted();
-    var showPopup = function(title, type) {
-      if (alertPopup === null) {
-        alertPopup = $ionicPopup.alert({
+    var showPopup = function( title, type ) {
+      if ( alertPopup === null ) {
+        alertPopup = $ionicPopup.alert( {
           title: title,
           okType: 'button-' + type
-        });
+        } );
       } else {
         alertPopup.close();
         alertPopup = null;
       }
     };
-    var showList = function() {};
-    $scope.returnMain = function() {
-      $state.go('index.main', {}, {
-        reload: true
-      });
+    var ShowDailyCompleted = function() {
+      $ionicPlatform.ready( function() {
+        if ( !ENV.fromWeb ) {
+          $cordovaSQLite.execute( db, "SELECT * FROM Csbk1  where  DriverId='" + sessionStorage.getItem( "strDriverId" ) + "' and CompletedDate='" + $scope.Search.CompletedDate + "' " )
+            .then(
+              function( results ) {
+                $scope.Csbk1s = new Array();
+                if ( results.rows.length > 0 ) {
+                  var jobs = '';
+                  for ( var i = 0; i < results.rows.length; i++ ) {
+                    var Csbk1_acc = results.rows.item( i );
+                    jobs = {
+                      bookingno: Csbk1_acc.BookingNo,
+                      JobNo: Csbk1_acc.JobNo,
+                      CollectedAmt: Csbk1_acc.CollectedAmt
+                    };
+                    $scope.Csbk1s.push( jobs );
+                  }
+                  if ( window.cordova && window.cordova.plugins.Keyboard ) {
+                    cordova.plugins.Keyboard.close();
+                  }
+                } else {}
+              },
+              function( error ) {}
+            );
+        } else {}
+      } );
     };
-    $scope.save = function() {};
-    $scope.clear = function() {};
-    var ipObj1 = {
-      callback: function(val) { //Mandatory
-        // console.log('Return value from the datepicker popup is : ' + val, new Date(val));
-        $scope.Search.CompletedDate = moment(new Date(val)).format('YYYYMMDD');
-ShowDailyCompleted();
-      }
+    $scope.returnMain = function() {
+      $state.go( 'index.main', {}, {
+        reload: true
+      } );
     };
     $scope.OnDatePicker = function() {
-      ionicDatePicker.openDatePicker(ipObj1);
-
-  };
-
-
-
-
-    $('#idCompletedDate').on('keydown', function(e) {
-      if (e.which === 9 || e.which === 13) {
-        if (window.cordova) {
-          $cordovaKeyboard.close();
-
+      var ipObj1 = {
+        callback: function( val ) { //Mandatory
+          // console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+          $scope.Search.CompletedDate = moment( new Date( val ) ).format( 'YYYYMMDD' );
+          ShowDailyCompleted();
         }
-        if (alertPopup === null) {} else {
-          alertPopup.close();
-          alertPopup = null;
-        }
-      }
-    });
-    showList();
+      };
+      ionicDatePicker.openDatePicker( ipObj1 );
+    };
+    ShowDailyCompleted();
   }
-]);
+] );
