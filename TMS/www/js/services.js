29ff16b3946e62351da61e2ff18cd796208f27ed
vivlong <vivlonglz@gmail.com>
2016-07-10 16:06:35 +0800

    Add DownloadService

--- TMS/www/js/services.js
+++ TMS/www/js/services.js
@@ -448,3 +448,61 @@ appServices.service( 'SqlService', [ '$q', 'ENV', '$timeout', '$ionicLoading', '
         };
     }
 ] );
+
+appServices.service( 'DownloadFileService', [ 'ENV', '$http', '$timeout', '$ionicLoading', '$cordovaToast', '$cordovaFile', '$cordovaFileTransfer', '$cordovaFileOpener2',
+    function( ENV, $http, $timeout, $ionicLoading, $cordovaToast, $cordovaFile, $cordovaFileTransfer, $cordovaFileOpener2 ) {
+        this.Download = function( url, fileName, fileType, onPlatformError, onCheckError, onDownloadError ) {
+            $ionicLoading.show( {
+                template: "Download  0%"
+            } );
+            var blnError = false;
+            if ( !ENV.fromWeb ) {
+                $cordovaFile.checkFile( cordova.file.externalRootDirectory + '/' + ENV.rootPath, fileName )
+                    .then( function( success ) {
+                        //
+                    }, function( error ) {
+                        blnError = true;
+                    } ).catch( function( ex ) {
+                        console.log( ex );
+                    } );
+                var targetPath = cordova.file.externalRootDirectory + '/' + ENV.rootPath + '/' + fileName;
+                var trustHosts = true;
+                var options = {};
+                if ( !blnError ) {
+                    $cordovaFileTransfer.download( url, targetPath, trustHosts, options ).then( function( result ) {
+                        $ionicLoading.hide();
+                        $cordovaFileOpener2.open( targetPath, fileType ).then( function() {
+                            // success
+                        }, function( err ) {
+                            // error
+                        } ).catch( function( ex ) {
+                            console.log( ex );
+                        } );
+                    }, function( err ) {
+                        $cordovaToast.showShortCenter( 'Download faild.' );
+                        $ionicLoading.hide();
+                        if ( onDownloadError ) onDownloadError();
+                    }, function( progress ) {
+                        $timeout( function() {
+                            var downloadProgress = ( progress.loaded / progress.total ) * 100;
+                            $ionicLoading.show( {
+                                template: "Download  " + Math.floor( downloadProgress ) + "%"
+                            } );
+                            if ( downloadProgress > 99 ) {
+                                $ionicLoading.hide();
+                            }
+                        } )
+                    } ).catch( function( ex ) {
+                        console.log( ex );
+                    } );
+                } else {
+                    $ionicLoading.hide();
+                    $cordovaToast.showShortCenter( 'Check file faild.' );
+                    if ( onCheckError ) onCheckError();
+                }
+            } else {
+                $ionicLoading.hide();
+                if ( onPlatformError ) onPlatformError( url );
+            }
+        };
+    } ] );
